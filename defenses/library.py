c7c468e7e605537ae23f2e3bbd7989070effc85c
RahulKiran.G <rahulkiran2222@gmail.com>
2026-07-03 23:11:27 +0530

    Update library.py

--- defenses/library.py
+++ defenses/library.py
@@ -5,10 +5,8 @@ class DefenseProtocols:
 
     @staticmethod
     def xml_tagging(user_input):
-        """Wraps user input in XML tags to help the model distinguish instructions."""
         return f"<user_input>\n{user_input}\n</user_input>"
 
     @staticmethod
     def delimiter_guard(user_input):
-        """Adds clear delimiters."""
-        return f"### USER INPUT START ###\n{user_input}\n### USER INPUT END ###"
+        return f"### START INPUT ###\n{user_input}\n### END INPUT ###"
